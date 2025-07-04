a4e66e789dbf7b5e8b8d25311aece8f342c14c3b
大福 <suzuka08242525@icloud.com>
2025-07-05 01:25:30 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const board = document.getElementById('game-board');
 
 // 絵文字ペア（同じ絵柄が2枚ずつ）
 const cards = ['🍎', '🍌', '🍇', '🍊', '🍎', '🍌', '🍇', '🍊'];
+let flippedCards = [];  //今めくっているカード２枚保持
+let lock = false;       //めくり中の操作制限
+
 
 // カードを1枚ずつ作る
 cards.forEach((emoji, index) => {
@@ -11,10 +14,35 @@ cards.forEach((emoji, index) => {
   card.dataset.emoji = emoji;
   card.dataset.index = index;
 
-  // カードをクリックしたとき
+  /*//カードをクリックしたとき
   card.addEventListener('click', () => {
     card.textContent = emoji; // めくる
   });
+  */
+
+  card.addEventListener('click', () => {
+    if (lock) return;          //処理ロック中は無視
+    if (flippedCards.includes(card)) return;  //同じカードは無視
+    if (card.textContent !== '❓') return;   //すでにめくってるカードは無視
+
+    card.textContent = emoji; // めくる
+    flippedCards.push(card);
+
+    if (flippedCards.length === 2){
+        lock = true;  //二枚めくったら操作をロック
+
+        if (flippedCards[0].dataset.emoji === flippedCards[1].dataset.emoji){
+            flippedCards = [];
+            lock = false;
+        } else {
+            setTimeout (() => {
+                flippedCards.forEach(c => c.textContent = '❓');
+                flippedCards = [];
+                lock = false;
+            }, 500);
+        }
+    }
+  })
 
   board.appendChild(card);
 });
